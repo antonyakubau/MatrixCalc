aac1b9ed50fa3add30beed5d07b3a63cabf00dee
antonyakubau <117779437+antonyakubau@users.noreply.github.com>
2023-09-18 05:34:39 +0200

    Revert "Values of entries are saved"

--- MatrixCalc/MatrixCalc/Model/InputEntry.cs
+++ MatrixCalc/MatrixCalc/Model/InputEntry.cs
@@ -7,10 +7,8 @@ namespace MatrixCalc.Model
 	public class InputEntry : BaseEntry 
 	{
         private Random random = new Random();
-        private string lastNumber;
-        private string oldNumber;
 
-        public int Row { get; set; }
+		public int Row { get; set; }
 		public int Column { get; set; }
         public int LineId { get; set; }
 
@@ -19,10 +17,9 @@ namespace MatrixCalc.Model
         { 
             Keyboard = Keyboard.Numeric;
             MaxLength = 3;
-            Text = random.Next(0, 999).ToString();
+            Text = random.Next(1, 999).ToString();
             TextChanged += UpdateResults;
             FontManager.UpdateFontDelegate += UpdateFontSize;
-            Unfocused += RestoreNumber;
         }
 
         public void UpdateResults(object sender, TextChangedEventArgs e)
@@ -34,19 +31,15 @@ namespace MatrixCalc.Model
             }
             else
             {
-                lastNumber = e.OldTextValue;
+                Text = e.OldTextValue;
             }
 
         }
 
+
         public void UpdateFontSize()
         {
             FontSize = Matrix.ChildHeight / 3.5;
-        }
-
-        public void GenerateNewValue()
-        {
-            Text = random.Next(0, 999).ToString();
         }
 
         private bool IsNumeric(string value)
@@ -67,13 +60,6 @@ namespace MatrixCalc.Model
             return true;
         }
 
-
-        private void RestoreNumber(object sender, FocusEventArgs e)
-        {
-            if (Text == "")
-                Text = lastNumber;
-
-        }
 
         private bool SumMoreZero(string value)
         {
